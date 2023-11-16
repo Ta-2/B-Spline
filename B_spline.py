d4085c5df27bb150f1a076dfc66d998dce895fae
tatuya <nakao.tatuya.2000@gmail.com>
2023-11-17 00:18:53 +0900

    modified B_spline

--- B_spline.py
+++ B_spline.py
@@ -14,12 +14,11 @@ if close:
     control_vec = np.linspace(1.0, 1.0, control_size)
     control_vec = np.random.rand(control_size)
     knot_vec = np.linspace(0.0, 1.0, knot_size)
-    dif_bef, dif_aft = (knot_vec[1]-knot_vec[0]), (knot_vec[-1]-knot_vec[-2])
 
     control_vec = np.append([control_vec[0]]*(dim-1), control_vec)
     control_vec = np.append(control_vec, [control_vec[-1]]*(dim-1))
-    knot_vec = np.append([knot_vec[0]-dif_bef]*dim, knot_vec)
-    knot_vec = np.append(knot_vec, [knot_vec[-1]+dif_aft]*dim)
+    knot_vec = np.append([knot_vec[0]]*dim, knot_vec)
+    knot_vec = np.append(knot_vec, [knot_vec[-1]]*dim)
 else:
     control_vec = np.linspace(1.0, 1.0, control_size)
     knot_vec = np.linspace(0.0, 1.0, knot_size)
@@ -40,20 +39,9 @@ def k_index(k):
     if knot_vec[-1] == k:
         return knot_size-1
 
-base_dict = {}
-for idx, knot in enumerate(knot_vec[:-(dim+1)]):
-    if knot not in base_dict.keys():
-        base_dict[knot] = [idx]
-    else:
-        base_dict[knot].append(idx)
-print(base_dict)
-
 def base_func(j, t, k, close=False):
     t_idx = k_index(t)
-    #print(str(t) + ", " + str(t_idx) + ", " + str(j) + ", " + str(k))
     if k == 0:
-        #if close and t==1.0 and j==control_size-2:
-        #    return 1.0
         if j <= t_idx and t_idx < j+1:
             return 1.0
         else:
@@ -77,17 +65,17 @@ def base_func(j, t, k, close=False):
 
 def B_spline(t, base_num=-1, close=False):
     curve = 0.0
-    base = 0.0
     if base_num == -1:
-        #for i in range(control_size-1):
         for i in range(control_size):
-            curve += control_vec[i]*base_func(i, t, dim, close)
+            if t == 1.0 and i==control_size-1:
+                curve += control_vec[i]
+            else:
+                curve += control_vec[i]*base_func(i, t, dim, close)
     else:
-        #for i in list(base_dict.values())[base_num]:
-            #print(i)
-        #    base += base_func(i, t, dim, close)
-        #curve = control_vec[base_num]*base
-        curve = base_func(base_num, t, dim, close)
+        if t == 1.0 and base_num==control_size-1:
+            curve = 1.0
+        else:
+            curve = base_func(base_num, t, dim, close)
     return curve
 
 curve_num = 200
@@ -104,8 +92,7 @@ ax = plt.gcf().gca()
 ax.set_xticks(knot_vec)
 ax.set_xlim(0.0, 1.0)
 ax.set_yticks(control_vec)
-#ax.set_yticks(np.array([0.0, 0.25, 0.5, 0.75, 1.0]))
-#ax.set_ylim([-0.1, 1.1])
+
 ax.grid()
 plt.plot(x, y)
 for b in bases:
